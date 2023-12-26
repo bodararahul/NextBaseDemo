f0e3ffbad8f2ef1bf120040a914ade3ece2550c8
bodararahul <51812823+bodararahul@users.noreply.github.com>
2023-12-26 16:28:30 +0530

    Update page.tsx

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -7,9 +7,14 @@ import { useRouter } from 'next/navigation';
 export default function Logout() {
   const router = useRouter();
   useDidMount(async () => {
-    await supabaseUserClientComponentClient.auth.signOut();
-    router.refresh();
-    router.replace('/');
+    try {
+      await supabaseUserClientComponentClient.auth.signOut();
+
+      router.refresh();
+      router.replace('/login');
+    } catch (error) {
+      console.error('Error during sign-out:', error);
+    }
   });
 
   return <T.P>Signing out...</T.P>;
